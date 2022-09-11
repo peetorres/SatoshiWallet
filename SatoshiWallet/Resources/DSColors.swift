bd974bc89868f4301da9fb7633b9c777e9267f8c
Pedro Gabriel <pee.torres@gmail.com>
2022-09-11 22:09:50 +0100

    Start using design system colors

---
+++ SatoshiWallet/Resources/DSColors.swift
@@ -0,0 +1,25 @@
+//
+//  DSColors.swift
+//  SatoshiWallet
+//
+//  Created by Pedro Gabriel on 11/09/22.
+//
+
+import UIKit
+
+extension UIColor {
+    /// Any: AppWhite , Dark: AppBlack
+    static var appBackgroundColor = UIColor(named: "AppBackgroundColor")
+
+    /// Any: AppWhite , Dark: AppBlack
+    static var appSecondaryBackgroundColor = UIColor(named: "AppSecondaryBackgroundColor")
+
+    /// Any: AppBlack , Dark: AppWhite
+    static var appLabelColor = UIColor(named: "AppLabelColor")
+
+    /// Hex: #5c9b1c
+    static var appGreen = UIColor(named: "AppGreen")
+
+    /// Hex: #ff4b4b
+    static var appRed = UIColor(named: "AppRed")
+}
