9aebdcd0f0ea5b77ab61507cad7ec6c3ad014a55
Pedro Gabriel <pee.torres@gmail.com>
2022-09-13 20:52:40 +0100

    Create ServicesEmptyStub

---
+++ SatoshiWalletTests/Scenes/List/Stubs/ListServicesEmptySuccessStub.swift
@@ -0,0 +1,27 @@
+//
+//  ListServicesEmptySuccessStub.swift
+//  SatoshiWalletTests
+//
+//  Created by Pedro Gabriel on 13/09/22.
+//
+
+import Foundation
+@testable import SatoshiWallet
+
+class ListServicesEmptySuccessStub: ListServicesProtocol {
+    func getCryptoList(limit: Int,
+                       completion: @escaping ((Result<[Crypto], NetworkError>) -> Void)) {
+        let cryptos: [Crypto] = []
+        completion(.success(cryptos))
+    }
+
+    func getAssetList(limit: Int,
+                      completion: @escaping ((Result<ListResponse, NetworkError>) -> Void)) {
+        completion(.success(.init(data: [], timestamp: 1)))
+    }
+
+    func getTickerList(tickers: [String],
+                       completion: @escaping ((Result<[Ticker], NetworkError>) -> Void)) {
+        completion(.success([]))
+    }
+}
