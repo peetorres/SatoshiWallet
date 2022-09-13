55086fbb02d34425ecabd237b12f537e2e6f4521
Pedro Gabriel <pee.torres@gmail.com>
2022-09-13 11:20:54 +0100

    Implement ListViewModelTests with SuccessStub

--- SatoshiWalletTests/Scenes/List/ListViewModelTests.swift
+++ SatoshiWalletTests/Scenes/List/ListViewModelTests.swift
@@ -9,12 +9,33 @@ import XCTest
 @testable import SatoshiWallet
 
 class ListViewModelTests: XCTestCase {
-//    var sut: ListViewModel!
+    // MARK: Properties
+    var sut: ListViewModel!
 
+    // MARK: Setup
     override func setUpWithError() throws {
         try super.setUpWithError()
-//        sut = ListViewModel(service: ListServicesSuccessStub())
     }
 
-    func testServiceSuccessHandler() {}
+    // MARK: Test Methods
+    func testServiceSuccessHandlerCryptosCounting() {
+        sut = ListViewModel(service: ListServicesSuccessStub())
+        sut.getCryptoList(isBackgroundFetch: false)
+        XCTAssertEqual(sut.cryptos?.count, 4)
+    }
+
+    func testServiceSuccessHandlerMutableCryptosWithoutSearch() {
+        sut = ListViewModel(service: ListServicesSuccessStub())
+        sut.getCryptoList(isBackgroundFetch: false)
+        XCTAssertEqual(sut.mutableCryptos?.count, 4)
+    }
+
+    func testServiceSuccessHandlerMutableCryptosWithSearch() {
+        sut = ListViewModel(service: ListServicesSuccessStub())
+        sut.getCryptoList(isBackgroundFetch: false)
+
+        sut.searchText = "T"
+
+        XCTAssertEqual(sut.mutableCryptos?.count, 2)
+    }
 }
